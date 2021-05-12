d9a12c4422098224e44167e9d5080f64dbd4ed87
ConsciosSalt <johndoe@example.com>
2021-05-12 14:12:54 +0300

    update timer behavior

--- src/App.js
+++ src/App.js
@@ -1,80 +1,93 @@
 import React, { useState, useEffect } from "react";
 
-const PRESET_ARRAY = [{name:"30 Secs",value:30}, {name:"Work 5",value:300}, {name:"Quick 15",value:900}, {name:"30 min",value:1800}, {name:"One hour", value:3600}];
+const PRESET_ARRAY = [
+  { name: "30 Secs", value: 30 },
+  { name: "Work 5", value: 300 },
+  { name: "Quick 15", value: 900 },
+  { name: "30 min", value: 1800 },
+  { name: "One hour", value: 3600 },
+  { name: "Two hours", value: 7200 },
+];
 let countdown;
+let counter;
+let stopped = false;
 
 function App() {
-  const [navigationVisible, setNavigationVisible] = useState(false);
-  const [timerStopped, setTimerStopped] = useState(true);
-
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-
-  const [endTime, setEndTime] = useState(0);
-
-  useEffect(() => {
-    console.log(endTime);
-  }, [endTime]);
-
-  function navigationContainerClickHandler() {
-    setNavigationVisible(!navigationVisible);
-  }
-
-  function timerControlsHandler() {
-    if (!timerStopped) {
-      clearInterval(countdown);
-    } else {
-      countdown = setInterval(() => {
-        // const now = new Date(Date.now());
-        // setSeconds(now.getSeconds());
-        // setMinutes(now.getMinutes());
-        // setHours(now.getHours());
-        updateTimer();
-      }, 1000);
-    };
-
-    setTimerStopped(!timerStopped);
-  }
+  const [insertedSeconds, setInsertedSeconds] = useState(0);
+  const [remainSeconds, setRemainSeconds] = useState(0);
+  const [controlTitle, setControlTitle] = useState('play');
+  const[finishDate, setFinishDate] = useState("");
 
   function updateTimer() {
-    let secondsLeft = Math.round((endTime - Date.now()) / 1000);
-    if(secondsLeft < 0){ 
-        // if (allowSound){
-        //     snd.play();
-        // }   
-        setTimerStopped(true);
-        return;
+    setRemainSeconds(--counter);
+    setFinishDate(new Date(Date.now()+counter*1000).toLocaleString());
+    if (counter<=0){
+      setControlTitle('Play');
     }
-    updateTimeLeft(secondsLeft);  
   }
 
-  function updateTimeLeft(seconds) {
-    let hours = (seconds / 360 < 1 ? Math.floor(seconds / 360): null) ;
-    let reminderMinutes = hours % 360;
-    let minutes = Math.floor(reminderMinutes / 60);
-    let remindSeconds = reminderMinutes % 60;
-    setHours(hours);
-    setMinutes(minutes);
-    setSeconds(remindSeconds);
+  function stopTimer() {
+    clearInterval(countdown);
+    countdown = undefined;
   }
+
+  function startTimer() {
+    countdown = setInterval(updateTimer, 1000);
+  }
+
+  useEffect(() => {
+    if (countdown !== undefined && counter <= 0) {
+      stopTimer();
+    }
+
+    if (countdown === undefined && counter > 0) {
+      startTimer();
+    }
+
+    return () => {
+      clearInterval(countdown);
+      countdown = undefined;
+    };
+  }, [remainSeconds]);
 
   return (
     <div className="App">
-      <header>
-        <NavigationHeaderContainer
-          buttonHandler={navigationContainerClickHandler}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (countdown !== undefined){
+            clearInterval(countdown);
+            countdown = undefined;
+            stopped   = true;
+            setControlTitle('Play');
+            setFinishDate("");
+          }else{
+            if (stopped) {
+              stopped = false;
+              startTimer();
+            }else{
+              counter = insertedSeconds;
+              setRemainSeconds(insertedSeconds);
+            }
+            setControlTitle('Stop'); 
+          }
+        }}
+      >
+        <input
+          type="number"
+          value={insertedSeconds}
+          onChange={(e) => setInsertedSeconds(parseInt(e.target.value))}
         />
-        <NameContainer />
-        <SettingsIconContainer />
-      </header>
+        <input type="submit" value={controlTitle}/>
+      </form>
       <div className="content">
-        <PresetNavigation navigationVisible={navigationVisible} setEndTime={setEndTime}/>
         <main>
           <div className="controls-container">
             <div className="clock-face-container-outer">
               <div className="clock-face-container-inner">
-                <ClockFaceDisplay
+                <p className={controlTitle==="Stop"?"":"blink"}>{remainSeconds}</p>
+                <p>{finishDate}</p>
+                {/* <ClockFaceDisplay
                   hours={hours}
                   minutes={minutes}
                   seconds={seconds}
@@ -82,7 +95,7 @@ function App() {
                 <ClockFaceControls
                   timerControlsHandler={timerControlsHandler}
                   timerStopped={timerStopped}
-                />
+                /> */}
               </div>
             </div>
             <div className="counter-container"></div>
@@ -123,7 +136,14 @@ function PresetNavigation(props) {
       {PRESET_ARRAY.map((preset, index) => {
         return (
           <div key={index} className="preset-container">
-            <button className="preset-container-button" onClick={()=>{props.setEndTime(preset.value) }}>{preset.name}</button>
+            <button
+              className="preset-container-button"
+              onClick={() => {
+                props.setEndTime(preset.value);
+              }}
+            >
+              {preset.name}
+            </button>
             <button className="preset-container-button-delete">
               <i className="fas fa-trash inactive"></i>
             </button>
@@ -135,30 +155,34 @@ function PresetNavigation(props) {
 }
 
 function ClockFaceDisplay(props) {
-  if (props.minutes) {
-    return (
-      <div className="clock-face-container-display">
-        <p>
-          <span>{(props.hours < 10 ? "0" : "") + props.hours}</span>:
-          <span>{(props.minutes < 10 ? "0" : "") + props.minutes}</span>:
-          <span>{(props.seconds < 10 ? "0" : "") + props.seconds}</span>
-        </p>
-      </div>
-    );
-  } else {
-    return null;
-  }
+  //if (props.minutes || props.seconds) {
+  return (
+    <div className="clock-face-container-display">
+      <p>
+        <span>{(props.hours < 10 ? "0" : "") + props.hours}</span>:
+        <span>{(props.minutes < 10 ? "0" : "") + props.minutes}</span>:
+        <span>{(props.seconds < 10 ? "0" : "") + props.seconds}</span>
+      </p>
+    </div>
+  );
+  // } else {
+  //   return null;
+  // }
 }
 
 function ClockFaceControls(props) {
-  let classes=['clock-face-container-controls-container'];
-  if (!props.timerStopped){
-    classes.push('stopped');
+  let classes = ["clock-face-container-controls-container"];
+  if (!props.timerStopped) {
+    classes.push("stopped");
   }
 
   return (
-    <div className={classes.join(' ')} onClick={props.timerControlsHandler}>
-      {props.timerStopped?<i className="fas fa-play"></i>:<i class="fas fa-pause"></i>}
+    <div className={classes.join(" ")} onClick={props.timerControlsHandler}>
+      {props.timerStopped ? (
+        <i className="fas fa-play"></i>
+      ) : (
+        <i className="fas fa-pause"></i>
+      )}
     </div>
   );
 }
